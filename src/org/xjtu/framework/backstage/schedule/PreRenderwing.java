1999b63d6ab1c2ddfd4a00f60a0e76e95aa7caa7
Thys <Thys@LAPTOP-BRKPDHQP>
2020-03-18 10:40:55 +0800

    更改预渲染方式-填写frameInfo.txt 预渲染指令添加cd到老引擎目录

--- src/org/xjtu/framework/backstage/schedule/PreRenderwing.java
+++ src/org/xjtu/framework/backstage/schedule/PreRenderwing.java
@@ -59,12 +59,12 @@ public class PreRenderwing  implements Runnable {
                         ArrayList<Integer> frameToPreRender = new ArrayList<Integer>();
                         frameToPreRender = JobServiceImpl.frameToPreRender;
                         
-                        //获取最大帧值
-                        String cmdToMaxFrame = "cd "+ filePath +" && cat frameMax.txt";
-    					ShellLocal shellToMaxFrame = new ShellLocal(cmdToMaxFrame);
-    					String result = shellToMaxFrame.executeCmd();
-    					result = result.trim();
-    					int frameMax = Integer.parseInt(result) + 1;
+//                        //获取最大帧值
+//                        String cmdToMaxFrame = "cd "+ filePath +" && cat frameMax.txt";
+//    					ShellLocal shellToMaxFrame = new ShellLocal(cmdToMaxFrame);
+//    					String result = shellToMaxFrame.executeCmd();
+//    					result = result.trim();
+//    					int frameMax = Integer.parseInt(result) + 1;
                         
     					//获取预渲染图片长宽
     					int height = jobs_render.get(0).getxResolution();
@@ -75,8 +75,8 @@ public class PreRenderwing  implements Runnable {
                         String FrameName="frame"+FrameMax+".xml";
                         log.info("最大的帧的名字是："+FrameName);
 
-                        ShellLocal shell=new ShellLocal( "cd "+filePath+" && ls *.xml");
-                        result=shell.executeCmd();
+                        ShellLocal shell=new ShellLocal( "cd " + filePath + " && ls *.xml");
+                        String result=shell.executeCmd();
                         result=result.trim();
 
                         log.info("result的内容是："+result);
@@ -87,37 +87,50 @@ public class PreRenderwing  implements Runnable {
                             log.info("当前的filesName是=" + filesName[i]);
                             if (filesName[i].equals(FrameName)){
                             	
+                            	//更新frameInfo.txt文件
+                            	//清空frameInfo.txt，填写文件总数
+                            	String clearCmd = "cat /dev/null > " + filePath + "/frameInfo.txt";
+        						String appendCmd = "echo '" + frameToPreRender.size() + "' > " + filePath + "/frameInfo.txt";
+        						ShellLocal shellToInfo = new ShellLocal();
+        		    			shellToInfo.setCmd(clearCmd + " && " + appendCmd);
+        						shellToInfo.executeCmd();
+                            	
                                 //复制需要预渲染的xml文件            					
                             	for (int j = 0 ; j < frameToPreRender.size() ; j ++) {
-                            		String cmdToCopy = "cp " + filePath + " frame" + frameToPreRender.get(j) + ".xml " + filePath + "/pre/frame" + frameMax + ".xml";
+                            		String cmdToCopy = "cp " + filePath + " frame" + frameToPreRender.get(j) + ".xml " + filePath + "/frame" + frameToPreRender.get(j) + "_pre.xml";
                 					ShellLocal shellToCopy = new ShellLocal(cmdToCopy);
                 					shellToCopy.executeCmd();
                 					
                 					//更改预渲染参数
-                                	ShellLocal ink = new ShellLocal();
-                                	String sampleCountCmd="sed -i 's;<integer name=\"sampleCount\" value=\".*\"/>;<integer name=\"sampleCount\" value=\"100\"/>;g' "+ filePath +"/pre/frame"+ frameMax +".xml";
-                					String heightCmd="sed -i 's;<integer name=\"height\" value=\".*\"/>;<integer name=\"height\" value=\""+ height +"\"/>;g' "+ filePath +"/pre/frame"+ frameMax +"xml";
-                					String widthCmd="sed -i 's;<integer name=\"width\" value=\".*\"/>;<integer name=\"width\" value=\""+ width +"\"/>;g' "+ filePath +"/pre/frame"+ frameMax +".xml";
-                					ink.setCmd(sampleCountCmd+" && "+heightCmd+" && "+widthCmd);
+                                	ShellLocal shellToChange = new ShellLocal();
+                                	String sampleCountCmd = "sed -i 's;<integer name=\"sampleCount\" value=\".*\"/>;<integer name=\"sampleCount\" value=\"100\"/>;g' "+ filePath +"/frame"+ frameToPreRender.get(j) +"_pre.xml";
+                					String heightCmd = "sed -i 's;<integer name=\"height\" value=\".*\"/>;<integer name=\"height\" value=\""+ height +"\"/>;g' "+ filePath +"/frame"+ frameToPreRender.get(j) +"_pre.xml";
+                					String widthCmd = "sed -i 's;<integer name=\"width\" value=\".*\"/>;<integer name=\"width\" value=\""+ width +"\"/>;g' "+ filePath +"/frame"+ frameToPreRender.get(j) +"_pre.xml";
+                					shellToChange.setCmd(sampleCountCmd + " && " + heightCmd + " && " + widthCmd);
                                 	//更改预渲染参数结束
+                					
+                					//添加xml文件名到frameInfo
+                					String cmdToAddXml = "echo 'frame" + frameToPreRender.get(j) + "_pre.xml' > " + filePath + "/frameInfo.txt";
+                					ShellLocal shellToAddxml = new ShellLocal(cmdToAddXml);
+                					shellToAddxml.executeCmd();
                             	}                            	
                             	//复制结束
                             	
-                            	
+                            	//更新frameInfo.txt文件结束
                             	
                             	//开始预渲染
                             	String renderInstruct="/home/export/online1/systest/swsdu/xijiao/RWing-demo/dist/PreRWing";
-            		    		String cmdToRender = "cd /home/export/online1/systest/swsdu/xijiao/RWing-demo";
+            		    		String cmdCD = "cd /home/export/online1/systest/swsdu/xijiao/RWing-demo";
             		    		String shenweiPbsCommand="";
             		    		
-            		    		shenweiPbsCommand="-cross -q " + fuWuListName + " -b -o /home/export/online1/systest/swsdu/xijiao/Outprint/deskroom-2043-Job-" + jobName + "_pre.out -J " + jobName + "_pre -n " + nodes + " -np 1 -sw3runarg \"-a 1\"  -host_stack 2600 -cross_size 20000 " + renderInstruct + " " + filePath + "/pre/";
+            		    		shenweiPbsCommand="-cross -q " + fuWuListName + " -b -o /home/export/online1/systest/swsdu/xijiao/Outprint/" + jobName + "_pre.out -J " + jobName + "_pre -n " + nodes + " -np 1 -sw3runarg \"-a 1\"  -host_stack 2600 -cross_size 20000 " + renderInstruct + " " + filePath;
             		    		//bsub            -cross -q q_sw_share            -b -o deskroom82                                                                               -J deskroom-Pre    -n 82           -np 1 -sw3runarg "-a 1"  -host_stack 2600 -cross_size 20000 ./dist/PreRWing  /home/export/online1/systest/swsdu/xijiao/Users/xjtu/deskroom120/	
-            		    		PbsExecute  pbs=new PbsExecute(cmdToRender + " && bsub " + shenweiPbsCommand);
-								stdout=pbs.executeCmd().trim();
-								log.info("return stdout is "+stdout);
-								log.info("bsub comand is "+shenweiPbsCommand);
-								if(stdout!=null&&stdout.length()>0)
-       					    	stdout=stdout.substring(stdout.indexOf('<')+1,stdout.indexOf('>'));
+            		    		PbsExecute  pbs=new PbsExecute(cmdCD + " && bsub " + shenweiPbsCommand);
+								stdout = pbs.executeCmd().trim();
+								log.info("return stdout is " + stdout);
+								log.info("bsub comand is " + shenweiPbsCommand);
+								if(stdout != null && stdout.length() > 0)
+       					    	stdout = stdout.substring(stdout.indexOf('<')+1,stdout.indexOf('>'));
                             	//预渲染结束
                             	
                                 log.info(filesName[i]+"和"+FrameName+" 相等哦"+"job_render的size为="+jobs_render.size());
